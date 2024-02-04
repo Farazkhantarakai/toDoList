66baeb521d43c157cc5abc45a9dcac1b143c616d
Farazkhan <farazkhantaraka@gmail.com>
2024-02-05 02:42:44 +0500

    initial setup

---
+++ controller/toDo.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+// this is used for json parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let items = [];
+
+app.get("/", (req, res) => {
+  res.render("toDo.ejs");
+});
+
+app.post("/postToDo", (req, res) => {
+  const data = req.body;
+
+  items.push(data);
+  console.log(items);
+
+  res.redirect("/");
+});
+
+app.listen(PORT, () => {
+  console.log(`server started ${PORT}`);
+});
